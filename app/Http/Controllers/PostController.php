81ad8a8839c14f7ae2b8213b5d2fea59f26d6545
ksenijacer <ksenijacerovic7@gmail.com>
2022-09-16 10:56:33 +0200

    CSDP-24 Fix: PostController edited and added route for PostController

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Post;
+use App\Http\Requests\StorePostRequest;
+use App\Http\Requests\UpdatePostRequest;
 
 class PostController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response('index');
+    }
+
 }
