0203e65cb7226158e628b1a1c8a6a88a43bbf12c
ksenijacer <ksenijacerovic7@gmail.com>
2022-09-19 11:28:10 +0200

    CSDP-28 Feat:Error handling

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -24,9 +24,6 @@ class PostController extends Controller
             throw new CustomException();
         }
 
-        // if (!isset($some)) {
-        //     throw new CustomException();
-        // }
         return view('posts.index', ['posts' => Post::all()]);
     }
 
