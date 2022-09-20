849f5610122b8e3c88e70f28225e84c06e5ef967
ksenijacer <ksenijacerovic7@gmail.com>
2022-09-20 10:04:22 +0200

    [CSDP-27] Fix: Resource route added

--- routes/web.php
+++ routes/web.php
@@ -46,15 +46,14 @@ Route::get('/register/{data}', [RegisterController::class, 'create']);
 
 
 
-Route::controller(PostController::class)->group(function () {
-    Route::get('posts', 'index')->name('posts.index');
-    Route::get('posts/create', 'create')->name('posts.create');
-    Route::post('posts', 'store')->name('posts.store');
-    Route::get('posts/{post}', 'show')->name('posts.show');
-    Route::get('posts/{post}/edit', 'edit')->name('posts.edit');
-    Route::put('posts/{post}', 'update')->name('posts.update');
-    Route::delete('posts/{post}', 'destroy')->name('posts.destroy');
-});
+Route::resource('posts', PostController::class);
+    // Route::get('posts/create', 'create')->name('posts.create');
+    // Route::post('posts', 'store')->name('posts.store');
+    // Route::get('posts/{post}', 'show')->name('posts.show');
+    // Route::get('posts/{post}/edit', 'edit')->name('posts.edit');
+    // Route::put('posts/{post}', 'update')->name('posts.update');
+    // Route::delete('posts/{post}', 'destroy')->name('posts.destroy');
+
 Auth::routes();
 
 Route::get('/home', [ HomeController::class, 'index'])->name('home');
